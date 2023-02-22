88b9087c7abc286684f5c7c5e94b29294cbb23d7
Maël Naccache Tüfekçi <contact@maeln.com>
2023-02-22 18:43:34 +0100

    wip: this is not the way to go. see logseq note

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use bytes::buf;
+use byteorder::ReadBytesExt;
 use clap::{Parser, Subcommand};
+use tracing::{error, info};
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{Read, Write, Cursor};
 use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{Sender, Receiver};
 use std::sync::{Arc, Mutex};
 use std::{net::UdpSocket, path::PathBuf};
 use thiserror::Error;
@@ -49,12 +50,73 @@ enum MarshallError {
 }
 
 enum Message {
+    // ID: 0
     Send { filename: String, parts: u32 },
+    // ID: 1
     Accept,
+    // ID: 2
     Part { id: u32, data: Vec<u8> },
+    // ID: 3
     Sync { ids: Vec<u32> },
+    // ID: 4
     Ack { ids: Vec<u32> },
+    // ID: 5
     Loss { ids: Vec<u32> },
+}
+
+impl Message {
+    fn parse(data: &[u8]) -> Result<Self, MarshallError> {
+        match data[0] {
+            0 => {
+                let mut reader = Cursor::new(data);
+                let parts = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                let string_size = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                let mut string_bytes: Vec<u8> = Vec::with_capacity(string_size as usize);
+                reader.read_exact(&mut string_bytes).map_err(|_| MarshallError::UnableToDeserialize)?;
+                let filename = String::from_utf8_lossy(&string_bytes).to_string();
+                return Ok(Message::Send { filename, parts })
+            }
+            1 => Ok(Message::Accept),
+            2 => {
+                let mut reader = Cursor::new(data);
+                let id = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                let mut buffer: Vec<u8> = Vec::with_capacity(data.len() - 4);
+                reader.read_exact(&mut buffer).map_err(|_| MarshallError::UnableToDeserialize)?;
+                return Ok(Message::Part { id, data: buffer })
+            },
+            3 => {
+                let mut reader = Cursor::new(data);
+                let size = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                let mut ids: Vec<u32> = Vec::with_capacity(size as usize);
+                for _ in 0..size {
+                    let id = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                    ids.push(id);
+                }
+                return Ok(Message::Sync { ids })
+            }
+            4 => {
+                let mut reader = Cursor::new(data);
+                let size = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                let mut ids: Vec<u32> = Vec::with_capacity(size as usize);
+                for _ in 0..size {
+                    let id = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                    ids.push(id);
+                }
+                return Ok(Message::Ack { ids })
+            }
+            5 => {
+                let mut reader = Cursor::new(data);
+                let size = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                let mut ids: Vec<u32> = Vec::with_capacity(size as usize);
+                for _ in 0..size {
+                    let id = reader.read_u32::<byteorder::BigEndian>().map_err(|_| MarshallError::UnableToDeserialize)?;
+                    ids.push(id);
+                }
+                return Ok(Message::Loss { ids })
+            }
+            _ => Err(MarshallError::UnableToDeserialize)
+        }
+    }
 }
 
 #[derive(Debug, Error)]
@@ -66,6 +128,8 @@ enum BufferError {
     EOF,
 }
 
+// TODO: SEE LOGSEQ NOTE !
+
 fn read_to_end(file: &mut File, channel: Sender<Vec<u8>>) {
     loop {
         let mut buf: Vec<u8> = Vec::with_capacity(BUF_CAPACITY);
@@ -74,20 +138,45 @@ fn read_to_end(file: &mut File, channel: Sender<Vec<u8>>) {
         match file.read(&mut buf) { 
             Ok(read) => {
                 if read == 0 {
-                    println!("Reached EOF.");
+                    info!("Reached EOF.");
                     break;
                 }
                 channel.send(buf).expect("Failed to send buffer to reader.");
             }
             Err(err) => {
-                println!("Error when reading file: {err}");
+                error!(error = ?err, "Error when reading file.");
                 panic!();
             },
         }
     }
 }
 
-// fn handle_client()
+fn handle_send(socket: Arc<UdpSocket>, channel: Receiver<Vec<u8>>, parts_waiting_ack: Arc<Mutex<Vec<u32>>>) {
+    let mut part_id: u32 = 0;
+    loop {
+        match channel.recv() {
+            Ok(data) => {
+                // MTU - 1 (message ID) - 4 (part id) - 4 (array size)
+                for chunk in data.chunks(MTU - 1 - 4 - 4) {
+                    let mut packet_data: Vec<u8> = Vec::with_capacity(MTU);
+                    packet_data.push(2);
+                    packet_data.extend(part_id.to_be_bytes());
+                    packet_data.extend(chunk.len().to_be_bytes());
+                    packet_data.extend(chunk);
+                    socket.send(&packet_data).expect("Could not send part to client.");
+                    {
+                        parts_waiting_ack.lock().expect("Could not lock ack array.").push(part_id);
+                    }
+                    part_id += 1;
+                }
+            },
+            Err(err) => {
+                error!(error = ?err, "Error when receiving from channel");
+                break;
+            },
+        }
+    }
+}
 
 fn send(ip: String, port: usize, file: &PathBuf) -> () {
     let disp_path = file.to_string_lossy();
