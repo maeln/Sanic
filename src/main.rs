9d79c9f970159d833be305f3500c62239e18eb01
Maël Naccache Tüfekçi <contact@maeln.com>
2023-02-22 12:18:49 +0100

    wip: add a function to read the file to the end and send chunks
    Supposed to be run in a thread.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-
-use std::{path::PathBuf, net::UdpSocket};
-use std::io::Write;
-use bytes::BufMut;
-use thiserror::Error;
+use bytes::buf;
 use clap::{Parser, Subcommand};
+use std::fs::File;
+use std::io::{Read, Write};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::mpsc::Sender;
+use std::sync::{Arc, Mutex};
+use std::{net::UdpSocket, path::PathBuf};
+use thiserror::Error;
 
-const MTU: u16 = 1500;
+const MTU: usize = 1500;
+const BUF_CAPACITY: usize = 8 * 1024 * 1024; // 8Mib
 
 #[derive(Parser)]
 #[command(name = "Sanic")]
@@ -19,11 +23,8 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Send { 
-        ip: String,
-        file: PathBuf,
-    },
-    Receive {}
+    Send { ip: String, file: PathBuf },
+    Receive {},
 }
 
 fn main() {
@@ -31,7 +32,7 @@ fn main() {
     match &cli.command {
         Commands::Send { ip, file } => {
             send(ip.clone(), 6666, file);
-        },
+        }
         Commands::Receive {} => {
             receive();
         }
@@ -47,82 +48,64 @@ enum MarshallError {
     OutOfSpace,
 }
 
-trait Serialize {
-    fn serialize(&self, buffer: &mut dyn BufMut) -> Result<(), MarshallError>;
-}
-
-trait Deserialize {
-    fn deserialize(data: &[u8]) -> Result<Self, MarshallError> where Self: Sized;
-}
-
 enum Message {
-    Send {
-        filename: String,
-        parts: u32,
-    },
+    Send { filename: String, parts: u32 },
     Accept,
-    Part {
-        id: u32,
-        data: Vec<u8>
-    },
-    Sync {
-        ids: Vec<u32>
-    },
-    Ack {
-        ids: Vec<u32>
-    },
-    Loss {
-        ids: Vec<u32>
-    }
+    Part { id: u32, data: Vec<u8> },
+    Sync { ids: Vec<u32> },
+    Ack { ids: Vec<u32> },
+    Loss { ids: Vec<u32> },
 }
 
-impl Serialize for Message {
-    fn serialize(&self, buf: &mut dyn BufMut) -> Result<(), MarshallError> {
-        match self {
-            Message::Send { filename, parts } => {
-                buf.put_u8(0); // Message ID
-                buf.put_u32(*parts); // Number of parts
-                buf.put_u32(filename.len() as u32); // Filename length (in octets, not number of char)
-                for b in filename.as_bytes() { // filename string.
-                    buf.put_u8(*b);
-                }
-                return Ok(());
-            },
-            Message::Accept => {
-                buf.put_u8(1);
-                return Ok(());
-            }
-            Message::Part { id, data } => {
-                let mut buf = Vec::with_capacity(1 + 4 + data.len());
-                buf.push(2);
-                buf.extend(id.to_be_bytes());
-                buf.extend(data);
-                return buf;
-            }
-            Message::Sync { ids } => {
-                let mut buf = Vec::with_capacity(ids.len() * 4);
+#[derive(Debug, Error)]
+enum BufferError {
+    #[error("unknown")]
+    Unknown,
 
+    #[error("Reached EOF")]
+    EOF,
+}
+
+fn read_to_end(file: &mut File, channel: Sender<Vec<u8>>) {
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(BUF_CAPACITY);
+        // NOTE: We might not fill the buffer fully
+        // We should keep track of the actual length of the buffer and shrink the buffer initial capacity if we always read less
+        match file.read(&mut buf) { 
+            Ok(read) => {
+                if read == 0 {
+                    println!("Reached EOF.");
+                    break;
+                }
+                channel.send(buf).expect("Failed to send buffer to reader.");
             }
-            _ => todo!()
+            Err(err) => {
+                println!("Error when reading file: {err}");
+                panic!();
+            },
         }
     }
 }
+
+// fn handle_client()
 
 fn send(ip: String, port: usize, file: &PathBuf) -> () {
     let disp_path = file.to_string_lossy();
     println!("Sending {disp_path} to {ip}:{port}");
     let socket = UdpSocket::bind("0.0.0.0:6667").expect("Could not bind socket");
-    socket.connect(format!("{ip}:{port}")).expect("Could not connect to receiver");
+    socket
+        .connect(format!("{ip}:{port}"))
+        .expect("Could not connect to receiver");
 
-    let mut write_buf: Vec<u8> = Vec::with_capacity(MTU as usize);
+    let mut write_buf: Vec<u8> = Vec::with_capacity(MTU);
     write!(&mut write_buf, "Hello!").expect("Could not write message");
-    
+
     match socket.send(&write_buf) {
         Ok(sent) => println!("Sent {sent} octets"),
         Err(err) => {
             println!("Error {err}");
             return;
-        },
+        }
     }
 
     println!("Finished");
@@ -131,14 +114,14 @@ fn send(ip: String, port: usize, file: &PathBuf) -> () {
 fn receive() -> () {
     let socket = UdpSocket::bind("0.0.0.0:6666").expect("Could not bind socket");
     println!("Listening at port {}", socket.local_addr().unwrap().port());
-    let mut read_buf: Vec<u8> = vec![0; MTU as usize];
+    let mut read_buf: Vec<u8> = vec![0; MTU];
     loop {
         match socket.recv(&mut read_buf) {
             Ok(size) => println!("Received {size} octets."),
             Err(err) => {
                 println!("Error {err}.");
                 return;
-            },
+            }
         }
     }
 
